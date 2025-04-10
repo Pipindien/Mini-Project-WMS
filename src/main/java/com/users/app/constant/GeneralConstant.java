20db608698ea3a151c6cab2430e3fbed9253c113
Siti Isyaroh <sitiisyaroh17@gmail.com>
2025-04-10 15:50:22 +0700

    Hapus mau ganti

--- src/main/java/com/users/app/constant/GeneralConstant.java
+++
@@ -1,7 +0,0 @@
-package com.users.app.constant;
-
-public class GeneralConstant {
-    public static final String LOG_ACVITIY_REGISTER = "Register User";
-    public static final String LOG_ACVITIY_LOGIN = "Login User";
-    public static final String LOG_ACVITIY_CHECK_TOKEN= "Validate Token";
-}
--- src/main/java/com/users/app/service/AuditTrailsService.java
+++
@@ -1,9 +0,0 @@
-package com.users.app.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.users.app.dto.AuditTrailsRequest;
-import com.users.app.dto.AuditTrailsResponse;
-
-public interface AuditTrailsService {
-    AuditTrailsResponse insertAuditTrails(AuditTrailsRequest auditTrailsRequest) throws JsonProcessingException;
-}
