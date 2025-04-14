e69d99730704e19b75a3f9182876c985f0fa22f3
HafidzPR <82770213+HafidzPR@users.noreply.github.com>
2025-04-14 12:18:16 +0700

    Add files via upload
    Clean install  Cite + React + TailwindCSS

---
+++ wms_fe_clean/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="container bg-red-500">
+        Clean install react + vite + tailwind
+      </div>
+    </div>
+  );
+};
+
+export default App;
